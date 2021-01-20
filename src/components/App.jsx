6b3cd22236c9141e6d6639d1a40e7097f8f04371
Junior627 <43441842+Junior627@users.noreply.github.com>
2021-01-20 18:17:32 -0500

    I completed all of the Day 1 requirements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,24 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  let questionNo = 0;
+  return (
+    <div className="app">
+      <h1>Trivia!</h1>
+
+      <h4>
+        <Question text={data[questionNo].question.text} />
+      </h4>
+      <NextQuestion />
+    </div>
+  );
 }
 
+function Question(props) {
+  return <div className="Question"> {props.text} </div>;
+}
+
+function NextQuestion() {
+  return <button> Click Me! </button>;
+}
 export default App;
